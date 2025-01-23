741ccdc7c0706df70aa54bb384121f722f3a728a
oscarlally <116345465+oscarlally@users.noreply.github.com>
2025-01-23 17:01:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import os
-from map_coords import coords
-from functions import create_df, create_area_animation
+from functions import get_filepaths, \
+                      create_df, \
+                      create_animation
 
-"""Note the glaring inconsistencies with naming due to inadequate redundant naming of multiple areas (particularly 
-zones 6 and 7)"""
+"""Note the glaring inconsistencies with naming due to inadequate 
+redundant naming of multiple areas (particularly zones 6 and 7)"""
 
 data_dir = f"{os.getcwd()}/Data/Alina"
+data_dir_2 = f"{os.getcwd()}/Data/Ryan"
 image_path = f"{os.getcwd()}/Data/Map.png"
 
-files = os.listdir(data_dir)
-filepaths = [f"{data_dir}/{x}" for x in files]
+dir_1_paths = get_filepaths(data_dir)
+dir_2_paths = get_filepaths(data_dir_2)
 
-df = create_df(filepaths)
+id_1, df_1 = create_df(dir_1_paths)
+id_2, df_2 = create_df(dir_2_paths)
 
-print('Dataframe Successfully Created.')
+print('Dataframe(s) Successfully Created.')
 
-create_area_animation(df, 30, image_path, coords)
+create_animation([df_1, df_2], 60, image_path, move=True)
 
 
 
